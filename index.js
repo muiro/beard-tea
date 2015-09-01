cfc9668ec8746ac967795c26e826cc13ef3ff245
muiro <wubaga@gmail.com>
2015-09-01 16:02:02 -0400

    Updated dependencies, added test shell

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 var koa = require('koa');
 var app = koa();
 
-app.use(function *(next){
-    var start = new Date;
-    yield next;
-    var ms = new Date - start;
-    this.set('X-Response-Time', ms + 'ms'); 
-});
+// x-response-time
 
 app.use(function *(next){
-    var start = new Date;
-    yield next;
-    var ms = new Date - start;
-    console.log('%s %s - %s', this.method, this.url, ms);
+  var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  this.set('X-Response-Time', ms + 'ms');
 });
+
+// logger
+
+app.use(function *(next){
+ var start = new Date;
+  yield next;
+  var ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+// response
 
 app.use(function *(){
-    this.body = 'Hello World';
+  this.body = 'Hello World';
 });
 
 app.listen(3000);
